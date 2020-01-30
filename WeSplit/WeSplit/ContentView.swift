11eab47d95520e49f0bc424d6e9b17202e3ada75
Ethan Holen <ethanholen@gmail.com>
2020-01-29 19:02:19 -0700

    WeSplit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ethan Holen on 1/29/20.
+//  Copyright © 2020 Ethan Holen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
