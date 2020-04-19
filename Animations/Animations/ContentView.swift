b17e3c5331df5c63658d392af8893b3e32710da2
Ethan Holen <ethanholen@gmail.com>
2020-04-19 09:48:08 -0600

    32

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Ethan Holen on 4/19/20.
+//  Copyright © 2020 Ethan Holen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 0.0
+
+    var body: some View {
+
+            Button("Tap Me") {
+                withAnimation(){
+                    self.animationAmount += 360
+                }
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+            
+        }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
