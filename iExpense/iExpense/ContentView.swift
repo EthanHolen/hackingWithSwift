d4acb44976f4c95274e4348e53de9a4bb1d9b975
Ethan Holen <ethanholen@gmail.com>
2020-04-23 23:13:07 -0600

    36

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,65 @@
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Ethan Holen on 4/21/20.
+//  Copyright © 2020 Ethan Holen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+
+
+
+
+
+struct ContentView: View {
+    
+    @ObservedObject var expenses = Expenses()
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
+    
+    
+    var body: some View {
+        
+        NavigationView {
+            List{
+                
+                ForEach(expenses.items){ item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)
+                }){
+                    Image(systemName: "plus")
+                }
+            )
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 11 Pro Max")
+    }
+}
+
